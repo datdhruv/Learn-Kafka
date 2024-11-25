3a17d77a7663c148071b4bc8207fd972557e8702
Dhruv <dhruvjain1952@gmail.com>
2024-11-25 23:19:13 +0200

    init

---
+++ consumer.js
@@ -0,0 +1,36 @@
+const { Kafka } = require("kafkajs")
+const msg = process.argv[2];
+run();
+async function run() {
+
+    try {
+        const kafka = new Kafka({
+            "clientId": "myapp",
+            "brokers": ["localhost:9092"]
+        })
+
+        const consumer = kafka.consumer({groupId: "test"});
+        console.log("I am connecting....");
+        await consumer.connect();
+        console.log("I am connected!");
+
+        consumer.subscribe({
+            topic: "Users",
+            fromBeginning: true
+        })
+        
+        await consumer.run({
+            eachMessage: async result => {
+                console.log(`RECV MSG ${result.message.value} on partition ${result.partition}`)
+            }
+        })
+    }
+
+    catch (ex) {
+        console.log(`something bad happened ${ex}`)
+    }
+
+    finally {
+
+    }
+}
---
+++ topic.js
@@ -0,0 +1,32 @@
+const { Kafka } = require("kafkajs")
+run();
+async function run() {
+
+    try {
+        const kafka = new Kafka({
+            "clientId": "myapp",
+            "brokers": ["localhost:9092"]
+        })
+
+        const admin = kafka.admin();
+        console.log("I am connecting....");
+        await admin.connect();
+        console.log("I am connected!");
+        await admin.createTopics({
+            "topics": [{
+                "topic": "Users",
+                "numPartitions": 2
+            }]
+        })
+        console.log("Created successfully");
+        await admin.disconnect();
+    }
+
+    catch (ex) {
+        console.log(`something bad happened ${ex}`)
+    }
+
+    finally {
+        process.exit(0);
+    }
+}
